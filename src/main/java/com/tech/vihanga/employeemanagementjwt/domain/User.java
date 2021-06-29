cfb541e66fc593381b8f2d4624e44961ab156b67
vihanga <liyanage.vihanga99@gmail.com>
2021-06-29 23:48:32 +0530

    userPrincipal Implemented

--- src/main/java/com/tech/vihanga/employeemanagementjwt/domain/User.java
+++ src/main/java/com/tech/vihanga/employeemanagementjwt/domain/User.java
@@ -2,7 +2,6 @@ package com.tech.vihanga.employeemanagementjwt.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
@@ -10,7 +9,6 @@ import java.io.Serializable;
 import java.util.Date;
 
 @Entity
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
@@ -23,6 +21,7 @@ public class User implements Serializable {
     private String firstName;
     private String lastName;
     private String username;
+    private String password;
     private String email;
     private String profileImageUrl;
     private Date lastLoginDate;
@@ -32,4 +31,125 @@ public class User implements Serializable {
     private String[] authorities;
     private boolean isActive;
     private boolean inNotLocked;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+
+    public void setProfileImageUrl(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    public Date getLastLoginDate() {
+        return lastLoginDate;
+    }
+
+    public void setLastLoginDate(Date lastLoginDate) {
+        this.lastLoginDate = lastLoginDate;
+    }
+
+    public Date getLastLoginDisplayDate() {
+        return lastLoginDisplayDate;
+    }
+
+    public void setLastLoginDisplayDate(Date lastLoginDisplayDate) {
+        this.lastLoginDisplayDate = lastLoginDisplayDate;
+    }
+
+    public Date getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(Date joinDate) {
+        this.joinDate = joinDate;
+    }
+
+    public String[] getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String[] roles) {
+        this.roles = roles;
+    }
+
+    public String[] getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(String[] authorities) {
+        this.authorities = authorities;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public boolean isInNotLocked() {
+        return inNotLocked;
+    }
+
+    public void setInNotLocked(boolean inNotLocked) {
+        this.inNotLocked = inNotLocked;
+    }
 }
--- src/main/java/com/tech/vihanga/employeemanagementjwt/domain/UserPrincipal.java
+++ src/main/java/com/tech/vihanga/employeemanagementjwt/domain/UserPrincipal.java
@@ -1,43 +1,54 @@
 package com.tech.vihanga.employeemanagementjwt.domain;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
 
 public class UserPrincipal implements UserDetails {
+
+    private User user;
+
+    public UserPrincipal(User user1) {
+        this.user = user1;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return stream(this.user.getAuthorities()).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return this.user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return this.user.getUsername();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return this.user.isInNotLocked();
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return this.user.isActive();
     }
 }
